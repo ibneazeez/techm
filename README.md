93517620aea4bba01fa46ef264f45dfaa686ea8f
Anees Ahmed <mdaneesahmed@gmail.com>
2017-09-27 18:06:27 +0530

    Calculator

--- README.md
+++ README.md
@@ -4,4 +4,8 @@
 
 1. Prime Numbers 0 100
 2. Non Prime Numbers 0 100
+3. Check Results by Using Methods
+4. Calculator with all Static Polymorphism all, float, float, int int, float int
+
+
 
---
+++ src/com/techm/java/classwork/Calculator.java
@@ -0,0 +1,42 @@
+package com.techm.java.classwork;
+
+public class Calculator {
+	
+	
+	public static void main(String[] args) {
+		
+		int a=10;
+		int b = 34;
+		
+		
+		System.out.println(add(a,b));
+		System.out.println(add(a,b,10));
+		
+		//checkResult(int m1, int m2, int m3)
+	}
+	
+	
+	// intigervalue two parameters 
+	private static int add(int a, int b){
+		int c = a+b;
+		
+		return c;
+	}
+	
+	private static int add(int a, int b, int c){
+		return a + b + c;
+	}
+	
+	private static float add(int a, int b, float c){
+		
+		return (a+b+c);
+	}
+	
+	
+	// Compile time polymorphism
+	
+	
+	
+	
+
+}
