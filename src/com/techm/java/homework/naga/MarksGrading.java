439a89adefb940eae04e264f7f4e15e76bbb1a72
nagendra <nagendra@nagendra-HP-Notebook>
2017-09-28 13:36:42 +0530

    final update

---
+++ src/com/techm/java/homework/naga/MarksGrading.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package com.techm.java.homework.naga;
+
+/**
+ * @author nagendra
+ *
+ */
+public class MarksGrading {
+
+	public static void main(String[] args) {
+
+		int eng = 49, math = 95, java = 99, python = 41;
+		int total = eng + math + java + python;
+		float per = total / 4;
+
+		if (eng >= 40 && math >= 40 && java >= 40 && python >= 40) {
+			System.out.println("Passed In All Subjects");
+		} else if (eng < 40) {
+			System.out.println("Failed in Eng");
+		}
+		if (math < 40) {
+			System.out.println("Falied in math");
+		}
+		if (java < 40) {
+			System.out.println("Failed in java");
+		}
+		if (python < 40) {
+			System.out.println("Failed in Python");
+		}
+
+		if (eng >= 40 && math >= 40 && java >= 40 && python >= 40)
+			if (per >= 70) {
+				System.out.println("First Class and Congrats");
+			} else if (per >= 50 && per < 70) {
+				System.out.println("Second Class and improve some more");
+			} else if (per >= 40 && per < 50) {
+				System.out.println("Just Passed and work hard");
+			}
+
+	}
+
+}
---
+++ src/com/techm/java/homework/naga/PrimeNonPrimeNumbers.java
@@ -0,0 +1,33 @@
+package com.techm.java.homework.naga;
+
+public class PrimeNonPrimeNumbers {
+
+	public static void main(String[] args) {
+		// boolean f=false;
+		for (int n = 1; n <= 50; n++)
+			isPrime(n);
+
+	}
+
+	public static void isPrime(int a) {
+
+		boolean f = false;
+		int i = a;
+		int j = 2;
+		if (a > j) {
+			for (j = 2; j < i; j++) {
+				if (i % j == 0) {
+					f = true;
+					break;
+				}
+			}
+		}
+		if (f == true|| i == 1) {
+			System.out.println(i + "--NonPrime");
+		} else {
+			System.out.println(i + "--Prime");
+		}
+
+	}
+
+}
