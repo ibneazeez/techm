4370346658719bd0a0903a88be2899f476a4b982
nagendra <nagendra@nagendra-HP-Notebook>
2017-09-28 13:24:51 +0530

    Committing Homework

---
+++ src/com/techm/java/homework/naga/MulTables.java
@@ -0,0 +1,17 @@
+package com.techm.java.homework.naga;
+
+public class MulTables {
+	public static void main(String[] args) {
+		System.out.println("Below are the Tables from 1 to 10 :");
+
+		int i = 1;
+
+		for (; i <= 10; i++){
+			System.out.println("Printing Table--"+i);
+			for (int j = 1; j <= 10; j++)
+				
+				System.out.println(i + "*" + j + " =" + i * j);
+
+	}
+	}
+}
