50160d5c2d3e4fd160c2b9fa76b7558ced42e829
ks942m <ks942m@HVDIVD18MIS1259.ITServices.sbc.com>
2017-09-28 16:53:50 +0530

    Done primeNumber task

---
+++ src/com/techm/java/homework/krishna/primeNumbers.java
@@ -0,0 +1,25 @@
+package com.techm.java.homework.krishna;
+
+public class primeNumbers {
+
+
+	public static void main(String args[]) {
+		 int i;
+	for (i=1;i<99;i++) {
+		int x=i%i;
+		int y=i%2;
+	
+		
+	
+		if (i==2) {
+			System.out.println(i);}
+		else if(x==0&&y==1) {
+						 System.out.println(i);
+			
+		
+			}}
+				System.out.println(i);
+			}	
+	}
+
+	
