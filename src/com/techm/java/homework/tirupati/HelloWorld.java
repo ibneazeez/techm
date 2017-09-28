55188c40cac1dc5008d9b3e734dc4639009a0126
user <user@Thiru>
2017-09-28 10:43:12 +0530

    Committing Hello world program

---
+++ src/com/techm/java/homework/tirupati/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.techm.java.homework.tirupati;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Hello World 
+		System.out.println("Hellow World");
+
+	}
+
+}
