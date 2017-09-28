0eb48422d3bd827685832b5054841e87f6edde16
user <user@Thiru>
2017-09-28 12:32:23 +0530

    Committing Prime Numbers Program

--- src/com/techm/java/homework/tirupati/PrimeNumbers.java
+++ src/com/techm/java/homework/tirupati/PrimeNumbers.java
@@ -4,7 +4,9 @@ public class PrimeNumbers {
 
 	public static void main(String[] args) {
 		// TODO Prime Numbers
+		
 		System.out.println("Prime numbers between 0 to 100");
+		
 		for (int a =2; a<=100;a++){
 			for (int b = 2; b<=a; b++){
 				if(a==b)
