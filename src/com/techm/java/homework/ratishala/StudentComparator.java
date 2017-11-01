ecaeb36c15dfde12809955c15dee8f1d093d16b7
RR00444186 <RR00444186@INCHCMPC07925.ATT.TechMahindra.com>
2017-11-01 17:41:58 +0530

    Sorting in Collections

---
+++ src/com/techm/java/homework/ratishala/StudentComparator.java
@@ -0,0 +1,14 @@
+package com.techm.java.homework.ratishala;
+
+public class StudentComparator { 
+		int rollno;  
+		String name;  
+		int age;  
+		Student(int rollno,String name,int age){  
+		this.rollno=rollno;  
+		this.name=name;  
+		this.age=age;  
+		}  
+		}  
+
+}
---
+++ src/com/techm/java/homework/ratishala/TestSort.java
@@ -0,0 +1,19 @@
+package com.techm.java.homework.ratishala;
+
+import java.util.*;  
+import java.io.*;  
+public class TestSort{  
+public static void main(String args[]){  
+ArrayList<Student> al=new ArrayList<Student>();  
+al.add(new Student(0,"Vijay",23));  
+al.add(new Student(06,"Ajay",27));  
+al.add(new Student(05,"Jai",2));  
+  
+Collections.sort(al);  
+for(Student st:al){  
+System.out.println(st.rollno+" "+st.name+" "+st.age);  
+}  
+}  
+}  
+ 
+
